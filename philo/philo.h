758ebadd55714f57efc46f3562412a8c45e784c1
Erantala <erantala@student.hive.fi>
2025-06-01 04:13:34 +0300

    Initial commit

---
+++ philo/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/01 02:34:35 by erantala          #+#    #+#             */
+/*   Updated: 2025/06/01 04:11:33 by erantala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdatomic.h>
+
+#endif
